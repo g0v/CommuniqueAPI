d2ae9e0b1d5d5505fffc3306d667818d3276e732
Lee <jessy1092@gmail.com>
2014-01-30 14:07:35 +0800

    add .editorconfig and config_test.js to test.
    Signed-off-by: Lee <jessy1092@gmail.com>

---
+++ config_test.js
@@ -0,0 +1,10 @@
+module.exports = {
+    hackpad: {
+        "site": "lee-test",
+        "client": "DpYBTaHcYcQ",
+        "secret": "rXBhGzSqRJWR75zqCX5CgXaTak6ne3sj"
+    },
+    padID: [
+        "QOEEpGYNXFw"
+    ]
+}
