f69263f65a753e90925202c3e3e8bfcbce050342
Lee <jessy1092@gmail.com>
2014-02-06 00:01:07 +0800

    - query by date - use express to get entry.   close #2
    Signed-off-by: Lee <jessy1092@gmail.com>

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
-var loader = require('./lib/loader');
-var dbClient = require('./lib/db');
-var filter = {'Tags':'g0v文化部'};
+var express = require('express');
 
-// loader.init();
+var loader = require('./lib/loader'),
+    dbClient = require('./lib/db');
 
-// loader.run();
+var app = express.createServer();
 
+var v = '1.0';
 
-dbClient.queryData(filter, function (result) {
-    console.dir(result);
+loader.init();
+loader.run();
+
+app.get('/api/' + v + '/entry/:tag', function (req, res) {
+    console.log(req.params.tag);
+    var filter = {};
+    if(req.params.tag != 'all')
+    {
+        filter['tags'] = req.params.tag;
+    }
+    // console.log('start' + req.query.start);
+    var date = {};
+    if (typeof(req.query.start) != 'undefined')
+    {
+        date['$gte'] = req.query.start;
+        filter['date'] = date;
+    }
+    if (typeof(req.query.end) != 'undefined')
+    {
+        date['$lte'] = req.query.end;
+        filter['date'] = date;
+    }
+
+    dbClient.queryData(filter, function (result) {
+        res.send(result);
+        console.log(result);
+    });
 });
+
+app.listen(3000);
+
