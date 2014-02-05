363fe86024ee493d81b0a96b2b8de16e7a199cfd
Lee <jessy1092@gmail.com>
2014-02-05 22:50:30 +0800

    Query db function by filter
    Signed-off-by: Lee <jessy1092@gmail.com>

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 var loader = require('./lib/loader');
+var dbClient = require('./lib/db');
+var filter = {'Tags':'g0v文化部'};
 
-loader.init();
+// loader.init();
 
-loader.run();
+// loader.run();
+
+
+dbClient.queryData(filter, function (result) {
+    console.dir(result);
+});
