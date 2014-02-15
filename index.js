8918a6d89e2cf66b6abe27d69880ea24bbca0485
Lee <jessy1092@gmail.com>
2014-02-15 23:01:45 +0800

    Parse Tag Description

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 var express = require('express');
 
-var loader = require('./lib/loader'),
-    dbClient = require('./lib/db');
+var loader    = require('./lib/loader');
+var tagloader = require('./lib/tagloader');
+var dbClient  = require('./lib/db');
 
 var app = express();
 
 var v = '1.0';
 var period = 1000 * 60 * 60;  // 1hr
+
+// tagloader.init();
+// tagloader.run();
 
 loader.init();
 loader.run();
