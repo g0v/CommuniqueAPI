c9d305b54397a49976247fb13499f42c1bb09c1a
Lee <jessy1092@gmail.com>
2014-03-04 23:52:05 +0800

    Add CORS Header, fix bug
    Signed-off-by: Lee <jessy1092@gmail.com>

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ setInterval(function () {
 
 app.all('/*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET");
+    next();
 });
 
 app.get('/api/' + v + '/entry/:tag', function (req, res) {
