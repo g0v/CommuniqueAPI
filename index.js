ab6b12f317997503dc314c040a39c5d9393eaf64
Lee <jessy1092@gmail.com>
2014-06-01 16:28:10 +0800

    Add tagParser.js to inherit parser.js . issue #20
    Signed-off-by: Lee <jessy1092@gmail.com>

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ var v2 = '2.0';
 var period    = 1000 * 60 * 60;  // 1hr
 var tagPeriod = 1000 * 60 * 60;
 
-tagloader.init();
+dbClient.init();
+
+tagloader.init(dbClient);
 tagloader.run();
 
 loader.init();
