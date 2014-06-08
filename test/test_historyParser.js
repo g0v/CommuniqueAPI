81f1f0190f9f07fe50241a21d629bd7a8a9b1ff0
Lee <jessy1092@gmail.com>
2014-06-08 22:02:39 +0800

    add test_tagParser.js and endOfParser listener. issue #4
    Signed-off-by: Lee <jessy1092@gmail.com>

--- test/test_historyParser.js
+++ test/test_historyParser.js
@@ -4,48 +4,39 @@ var config        = require('../lib/config');
 var should        = require('should');
 
 describe('history Parser', function () {
-    this.timeout(50000);
+    this.timeout(5000);
 
-    var historyParser = new HistoryParser();
-    var hackpadClient = new Hackpad(config.hackpad.client, config.hackpad.secret, config.hackpad);
+    it('should have correct keys and content', function (done) {
 
-    hackpadClient.revisions('i8tMqWAFi39', function (err,result) {
-        if (err) {
-            it('should not be error', function () {
-                err.should.not.be.an.Error;
-            });
-        } else {
-            historyParser.runParser('i8tMqWAFi39', result);
-        }
-    });
+        var historyParser = new HistoryParser();
+        var hackpadClient = new Hackpad(config.hackpad.client, config.hackpad.secret, config.hackpad);
 
-    it('should hava correct keys and content', function (done) {
-        var i = 0;
+        hackpadClient.revisions('i8tMqWAFi39', function (err,result) {
+            if (err) {
+                it('should not be error', function () {
+                    err.should.not.be.an.Error;
+                });
+            } else {
+                historyParser.runParser('i8tMqWAFi39', result);
+            }
+        });
 
         historyParser.on('insertHackpadHistory', function (padHistory) {
             padHistory.should.have.keys(['padID', 'history', 'editNum', 'authors', 'authorsNum', 'createTimeStamp']);
             padHistory.padID.should.be.equal('i8tMqWAFi39');
-            i++;
-            waitEmitter(3, i, done);
         });
 
         historyParser.on('insertHackpadData', function (data) {
             data.should.have.keys(['date', 'num']);
-            i++;
-            waitEmitter(3, i, done);
         });
 
         historyParser.on('insertHackpadAuthors', function (author) {
             author.should.have.keys(['name', 'editNum', 'pads']);
             author.pads.should.containEql('i8tMqWAFi39');
-            i++;
-            waitEmitter(3, i, done);
+        });
+
+        historyParser.on('endOfParser', function () {
+            done();
         });
     });
 });
-
-function waitEmitter (num, i, done) {
-    if (num == i) {
-        done();
-    }
-}
