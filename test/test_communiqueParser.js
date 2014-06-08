250ba134298182d9a099b48762deb3bacd9df748
Lee <jessy1092@gmail.com>
2014-06-08 22:27:36 +0800

    Add test_communiqueParser.js issue #4
    Signed-off-by: Lee <jessy1092@gmail.com>

---
+++ test/test_communiqueParser.js
@@ -0,0 +1,46 @@
+var Hackpad          = require('hackpad');
+var CommuniqueParser = require('../lib/communiqueParser');
+var config           = require('../lib/config');
+var should           = require('should');
+
+describe('Communique Parser', function () {
+    this.timeout(5000);
+
+    it('should have correct keys and content', function (done) {
+
+        var communiqueParser = new CommuniqueParser();
+        var hackpadClient    = new Hackpad(config.hackpad.client, config.hackpad.secret, config.hackpad);
+
+        hackpadClient.export('gliZZuXU5aW', "latest", "html", function (err, result) {
+            if(err) {
+                it('should not be error', function () {
+                    err.should.not.be.an.Error;
+                });
+            } else {
+                communiqueParser.runParser('gliZZuXU5aW', result);
+            }
+        });
+
+        communiqueParser.on('removeCommuniqueData', function (padID) {
+            padID.should.be.equal('gliZZuXU5aW');
+        });
+
+        communiqueParser.on('updateCommuniqueData', function (id, data) {
+            data.should.be.an.Object;
+        });
+
+        communiqueParser.on('insertCommuniqueData', function (data, getItemID) {
+            data.should.have.keys(['date', 'padID', 'tags', 'content', 'comment', 'urls']);
+            data.padID.should.be.equal('gliZZuXU5aW');
+            getItemID.should.be.an.Function;
+        });
+
+        communiqueParser.on('updateHackpadListTitle', function (padID, text) {
+            padID.should.be.equal('gliZZuXU5aW');
+        });
+
+        communiqueParser.on('endOfParser', function () {
+            done();
+        });
+    });
+});
