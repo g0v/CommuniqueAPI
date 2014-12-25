a0e4521747a4637752b9a7f0c06ecd975183b788
Lee <jessy1092@gmail.com>
2014-12-26 00:05:18 +0800

    Use mongodb driver 2.0.x
    Signed-off-by: Lee <jessy1092@gmail.com>

--- test/test_communiqueParser.js
+++ test/test_communiqueParser.js
@@ -11,18 +11,18 @@ describe('Communique Parser', function () {
         var communiqueParser = new CommuniqueParser();
         var hackpadClient    = new Hackpad(config.hackpad.client, config.hackpad.secret, config.hackpad);
 
-        hackpadClient.export('gliZZuXU5aW', "latest", "html", function (err, result) {
+        hackpadClient.export('1JvYgpElmHs', "latest", "html", function (err, result) {
             if(err) {
                 it('should not be error', function () {
                     err.should.not.be.an.Error;
                 });
             } else {
-                communiqueParser.runParser('gliZZuXU5aW', result);
+                communiqueParser.runParser('1JvYgpElmHs', result);
             }
         });
 
         communiqueParser.on('removeCommuniqueData', function (padID) {
-            padID.should.be.equal('gliZZuXU5aW');
+            padID.should.be.equal('1JvYgpElmHs');
         });
 
         communiqueParser.on('updateCommuniqueData', function (id, data) {
@@ -31,12 +31,12 @@ describe('Communique Parser', function () {
 
         communiqueParser.on('insertCommuniqueData', function (data, getItemID) {
             data.should.have.keys(['date', 'padID', 'tags', 'content', 'content_en', 'comment', 'urls']);
-            data.padID.should.be.equal('gliZZuXU5aW');
+            data.padID.should.be.equal('1JvYgpElmHs');
             getItemID.should.be.an.Function;
         });
 
         communiqueParser.on('updateHackpadListTitle', function (padID, text) {
-            padID.should.be.equal('gliZZuXU5aW');
+            padID.should.be.equal('1JvYgpElmHs');
         });
 
         communiqueParser.on('endOfParser', function () {
